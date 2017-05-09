1596dec4a039a6329f2d5107c45365d1df52d25a
juandanielgaleano <juandanielgaleano@hotmail.com>
2017-05-09 20:49:12 -0300

    Recuadra cadenas
    Metodo en c# que recuadra cadenas

---
+++ RecuadrarCadenas/SolucionRecuadrarCadena/RecuadrarCadena/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecuadrarCadena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena = "";
+            Console.WriteLine("Ingrese una cadena a recuadrar con asteriscos");
+            cadena = Console.ReadLine();
+            Recuadrar facultad = new Recuadrar();
+            facultad.recuadrar(cadena);
+        }
+    }
+}
---
+++ RecuadrarCadenas/SolucionRecuadrarCadena/RecuadrarCadena/Recuadrar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecuadrarCadena
+{
+    public class Recuadrar
+    {
+        public void recuadrar(string cadena)
+        {
+            int largo = 0;
+            largo = cadena.Length;
+            string asteriscos = "*";
+            string recuadro = "*";
+            for (int i = 0; i < largo; i++)
+            {
+                recuadro = recuadro + asteriscos;
+            }
+            Console.WriteLine(recuadro + "*");
+            Console.WriteLine("*" + cadena + "*");
+            Console.WriteLine(recuadro + "*");
+            Console.ReadKey();
+        }
+    }
+}
